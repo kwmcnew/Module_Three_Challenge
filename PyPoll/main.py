dd9164e0767ff75b5da00a5e3db8f3f09df38151
kwmcnew <monew@kyles-MacBook-Pro.local>
2023-07-02 21:09:53 -0400

    Final update to PyPoll exercise

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -22,4 +22,55 @@ with open(csvpath) as csvfile:
    
     # Read each row of data after the header
     for row in csvreader:
-        print(row)
+        
+        # Pull the candidate name from the third column of the file
+        candidate = row[2]
+
+        # Increment and calculate the total vote count
+        total_votes += 1
+
+        # Fill the empty dictionary with the candidates and their vote count
+        if candidate in candidates:
+            candidates[candidate] += 1
+        else:
+            candidates[candidate] = 1
+
+#Calculate the percentage of votes each candidate won
+candidate_percentages = {}
+# Syntax explanation: for "key", "value" in "dictionary".items() 
+for candidate, votes in candidates.items():
+    percentage = (votes / total_votes) * 100
+    # Syntax explanation: dictionary[new key] = new value
+    candidate_percentages[candidate] = percentage
+
+# Identify the winner of the election based on popular vote
+winner = max(candidates, key=candidates.get)
+
+# Print results
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------") 
+for candidate, votes in candidates.items(): 
+    percentage = candidate_percentages[candidate]
+    # Format percentage to round to three decimal places
+    print(f"{candidate}: {percentage:.3f}% ({votes})")  
+print("-------------------------")
+print(f"Winner: {winner}")
+print("-------------------------")
+
+# Export a text file with the results
+output_path = 'Module_Three_Challenge/PyPoll/Analysis/Analysis_Results_PyPoll.txt'
+with open(output_path, 'w') as file:
+    file.write("Election Results\n")
+    file.write("-------------------------\n")
+    file.write("Total Votes: " + str(total_votes) + "\n")
+    file.write("-------------------------\n")
+    for candidate, votes in candidates.items(): 
+        percentage = candidate_percentages[candidate]
+        # Format percentage to round to three decimal places
+        file.write(candidate + ": " + format(percentage, ".3f") + "% " + "(" + str(votes) + ")" + "\n")
+    file.write("-------------------------\n")
+    file.write("Winner: " + winner + "\n")
+    file.write("-------------------------\n")
+
